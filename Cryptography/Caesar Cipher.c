5b9b37d09aca16ea6fe2194089c45d7a48ae5939
fatpound <84704675+fatpound@users.noreply.github.com>
2022-06-30 22:36:05 +0300

    Add files via upload

---
+++ Cryptography/Caesar Cipher.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+	int i, key;
+	char text[50];
+	
+	gets(text);
+	strupr(text);
+	
+	printf("\nlutfen bir sayi giriniz > ");
+	scanf("%d", &key);
+	
+	for (i = 0; i < strlen(text); i++)
+	{
+		text[i] = (char) ((((int) text[i]) - 65 + key) % 26 + 65);
+	}
+	
+	printf("%s", text);
+	
+	getch();
+	return 0;
+}
