d3d82e2cdc5b5074485c8abefd68d322e96cce74
fatpound <84704675+fatpound@users.noreply.github.com>
2022-06-30 22:33:56 +0300

    Add files via upload

---
+++ BASIC-Like C.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define MAIN    main()
+
+#define INT        int
+#define STRING   char*
+
+#define IF         if(
+#define THEN        ){
+#define FI          ;}
+#define ELSE    }else{
+	
+#define WHILE   while(
+#define DO          ){
+#define OD          ;}
+
+#define BEGIN        INT MAIN {
+#define END          getch();return 0;}
+
+BEGIN
+END
