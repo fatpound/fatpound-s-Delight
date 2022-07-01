252fde7ea5fb230dcbecef4d76385eb04484edc0
fatpound <84704675+fatpound@users.noreply.github.com>
2022-07-01 10:17:54 +0300

    Update QuickSort.c

--- Sorting/QuickSort.c
+++ Sorting/QuickSort.c
@@ -1,33 +1,33 @@
 #define SWAP_P(x, y) (*x == *y ? 0 : (*x ^= *y ^= *x ^= *y))
 
-int Bolum(int *dizi, int left, int right)
+int Div(int *d, int left, int right)
 {
-	int i, pivot = dizi[right], index = left - 1;
+	int i, pivot = d[right], index = left - 1;
 	
 	for (i = left; i < right; i++)
 	{
 		if (dizi[i] <= pivot)
 		{
 			index++;
-			SWAP_P(&dizi[i], &dizi[index]);
+			SWAP_P(&d[i], &d[index]);
 		}
 	}
 	
 	index++;
-	SWAP_P(&dizi[right], &dizi[index]);
+	SWAP_P(&dizi[right], &d[index]);
 	
 	return index;
 }
 
-void QuickSort(int *dizi, int left, int right)
+void QuickSort(int *d, int left, int right)
 {
 	if (left >= right)
 	{
 		return;
 	}
 	
-	int v = Bolum(dizi, left, right);
+	int v = Div(dizi, left, right);
 	
-	QuickSort(dizi, left, v - 1);
-	QuickSort(dizi, v + 1, right);
+	QuickSort(d, left, v - 1);
+	QuickSort(d, v + 1, right);
 }
