3aeeef7cd9fd2ae6a1ff4ed1806fa47a74a5e5ee
fatpound <84704675+fatpound@users.noreply.github.com>
2022-11-27 17:19:26 +0300

    Update Comparing & Swapping Variables.c

--- Comparing & Swapping Variables.c
+++ Comparing & Swapping Variables.c
@@ -5,7 +5,7 @@
 #define MAX_XYZ(x, y, z)     (x > y ? (y > z ? x : (x > z ? x : z)) : (y > z ? y : z))
 #define MIN_XYZT(x, y, z, t) (x > y ? (y > z ? (z > t ? t : z) : (z > t ? (y > t ? t : y) : (x > t ? y : (x > y ? (y > z ? z : y) : (y > z ? (x > z ? z : x) : x))))) : (y > z ? (y > t ? (x > z ? (z > t ? t : z) : (z > t ? (x > t ? t : x) : x)) : (x > z ? z : x)) : (y > t ? (x > t ? t : x) : x)))
 
-#define SWAP1(x, y)  (x == y ? x = y : x ^= y ^= x ^= y)
+#define SWAP1(x, y)  (x == y ? 0 : x ^= y ^= x ^= y)
 #define SWAP2(x, y)  (x -= y = (x += y) - y)
 #define SWAP3(x, y)  (x /= y = (x *= y) / y)
 #define SWAP4(x, y)  (x = x + y - (y = x))
@@ -13,7 +13,7 @@
 
 void XorSwap(int *x, int *y)
 {
-	(*x != *y) ? (*x ^= *y ^= *x ^= *y) : (*x = *y);
+	(*x != *y) ? (*x ^= *y ^= *x ^= *y) : 0;
 }
 
 void AddSwap(int *x, int *y)
