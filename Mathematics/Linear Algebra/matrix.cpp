4cef7200553dccd2e702b5d1420af72d0c036c50
fatpound <84704675+fatpound@users.noreply.github.com>
2022-12-05 19:01:54 +0300

    Update matrix.cpp

--- Mathematics/Linear Algebra/matrix.cpp
+++ Mathematics/Linear Algebra/matrix.cpp
@@ -19,14 +19,13 @@ matrix::matrix(int row, int col)
 	_row = row;
 	_col = col;
 
-	_matrix = new int*[_row];
+	_matrix = new int* [_row];
 
 	for (int i = 0; i < _row; i++)
 	{
 		_matrix[i] = new int[_col];
 	}
 }
-
 matrix::matrix(matrix* source)
 {
 	srand(time(NULL));
@@ -49,15 +48,9 @@ matrix::matrix(matrix* source)
 		}
 	}
 }
-
 matrix::~matrix()
 {
-	for (int i = 0; i < _row; i++)
-	{
-		free((void*)_matrix[i]);
-	}
 
-	free((void*)_matrix);
 }
 
 void matrix::printRow(int row)
@@ -69,7 +62,6 @@ void matrix::printRow(int row)
 
 	cout << "\n";
 }
-
 void matrix::printCol(int col)
 {
 	for (int i = 0; i < _row; i++)
@@ -77,7 +69,6 @@ void matrix::printCol(int col)
 		cout << _matrix[i][col] << "\n";
 	}
 }
-
 void matrix::printAll()
 {
 	for (int i = 0; i < _row; i++, cout << "\n")
@@ -95,12 +86,10 @@ void matrix::setValue(int row, int col, int value)
 {
 	_matrix[row][col] = value;
 }
-
 void matrix::setRand(int row, int col, int max)
 {
 	_matrix[row][col] = rand() % ++max;
 }
-
 void matrix::fillValue(int value)
 {
 	for (int i = 0; i < _row; i++)
@@ -111,7 +100,6 @@ void matrix::fillValue(int value)
 		}
 	}
 }
-
 void matrix::fillRand(int max)
 {
 	max++;
@@ -147,10 +135,9 @@ void matrix::toUnit()
 		}
 	}
 }
-
 void matrix::transPose()
 {
-	int** temp_matrix = new int*[_col];
+	int** temp_matrix = new int* [_col];
 
 	for (int k = 0; k < _col; k++)
 	{
@@ -179,14 +166,13 @@ void matrix::addMatrix(matrix* second)
 	{
 		for (int j = 0; j < _col; j++)
 		{
-			if (second->_matrix[i][j] != 0)
+			if (_matrix[i][j] != 0 && second->_matrix[i][j] != 0)
 			{
 				_matrix[i][j] += second->_matrix[i][j];
 			}
 		}
 	}
 }
-
 void matrix::multiplyScalar(int k)
 {
 	if (k == 1)
@@ -200,16 +186,14 @@ void matrix::multiplyScalar(int k)
 		}
 	}
 }
-
 void matrix::multiplyMatrix(matrix* second)
 {
 	if (_col != second->_row)
 	{
-		cout << "Cannot multiplicate!\n";
 		return;
 	}
 
-	int** temp_matrix = new int*[_row];
+	int** temp_matrix = new int* [_row];
 
 	for (int t = 0; t < _row; t++)
 	{
@@ -254,7 +238,29 @@ void matrix::multiplyMatrix(matrix* second)
 	}
 }
 
-matrix matrix::returnMultipledMatrix(matrix* second)
+matrix matrix::AddedMatrix(matrix* second)
+{
+	if (_row != second->_row || _col != second->_col)
+	{
+		return NULL;
+	}
+
+	matrix newm = new matrix(this);
+
+	for (int i = 0; i < _row; i++)
+	{
+		for (int j = 0; j < _col; j++)
+		{
+			if (newm._matrix[i][j] != 0 && second->_matrix[i][j] != 0)
+			{
+				newm._matrix[i][j] += second->_matrix[i][j];
+			}
+		}
+	}
+
+	return newm;
+}
+matrix matrix::MultipledMatrix(matrix* second)
 {
 	if (_col != second->_row)
 	{
@@ -280,8 +286,7 @@ matrix matrix::returnMultipledMatrix(matrix* second)
 
 	return new_matrix;
 }
-
-matrix matrix::returnMinor(matrix* source, int row, int col)
+matrix matrix::Minor(matrix* source, int row, int col)
 {
 	matrix src = new matrix(source->_row - 1, source->_col - 1);
 
@@ -312,8 +317,7 @@ matrix matrix::returnMinor(matrix* source, int row, int col)
 
 	return src;
 }
-
-matrix matrix::returnAdj(matrix* source)
+matrix matrix::Adjacent(matrix* source)
 {
 	matrix newm = new matrix(source->_row, source->_col);
 
@@ -321,7 +325,7 @@ matrix matrix::returnAdj(matrix* source)
 	{
 		for (int j = 0; j < source->_col; j++)
 		{
-			matrix temp = source->returnMinor(source, i, j);
+			matrix temp = source->Minor(source, i, j);
 
 			newm.setValue(i, j, temp.calculateDeterminant(&temp) * ((i + j) % 2 == 0 ? 1 : -1));
 		}
@@ -336,7 +340,7 @@ int matrix::calculateSarrus()
 {
 	if (_row != _col || _row > 3)
 	{
-		return (int)0x80000000;
+		return INT32_MIN;
 	}
 
 	if (_row < 3)
@@ -363,12 +367,11 @@ int matrix::calculateSarrus()
 
 	return det;
 }
-
 int matrix::calculateDeterminant(matrix* source)
 {
 	if (source->_row != source->_col)
 	{
-		return (int)0x80000000;
+		return INT32_MIN;
 	}
 
 	if (source->_row < 4 && source->_col < 4)
@@ -380,10 +383,19 @@ int matrix::calculateDeterminant(matrix* source)
 
 	for (int i = 0; i < source->_row; i++)
 	{
-		matrix newm = returnMinor(source, i, 0);
+		matrix newm = Minor(source, i, 0);
 
 		det += source->_matrix[i][0] * calculateDeterminant(&newm) * (i % 2 == 0 ? 1 : -1);
 	}
 
 	return det;
+}
+
+matrix matrix::operator + (matrix& source)
+{
+	return AddedMatrix(&source);
+}
+matrix matrix::operator * (matrix& source)
+{
+	return MultipledMatrix(&source);
 }
