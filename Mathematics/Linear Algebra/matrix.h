0c000e612195fbbc56cb3941cc025775bab9b02d
fatpound <84704675+fatpound@users.noreply.github.com>
2023-02-14 14:16:25 +0300

    Update matrix.h

--- Mathematics/Linear Algebra/matrix.h
+++ Mathematics/Linear Algebra/matrix.h
@@ -1,48 +1,60 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <array>
+
+using std::vector, std::array;
 
 class matrix
 {
 private:
-	double** _matrix = NULL;
-	int _row = 0, _col = 0;
-	double determinant = INT32_MIN;
+    vector<vector<double>> _matrix;
+    int _row = 0;
+    int _col = 0;
+    double determinant = 999979; // A prime number
+
+    double calculateSarrus();
+    double calculateDeterminant();
 
 public:
-	matrix(int row, int col);
-	matrix(matrix* source);
-	matrix(double* arr, double length, int row, int col);
-	~matrix();
+    matrix(int row, int col);
+    matrix(matrix& source);
+    matrix(double* arr, double length, int row, int col);
 
-	void printAll();
-	void printRow(int row);
-	void printCol(int col);
+    template<size_t size>
+    matrix(array<double, size>, int row, int col);
 
-	void toUnit();
-	void setValue(int row, int col, double value);
-	void setRand(int row, int col, int max);
-	void fillValue(double value);
-	void fillRand(int max);
+    ~matrix();
 
-	void transPose();
-	void addMatrix(matrix* second);
-	void subtractMatrix(matrix* second);
-	void multiplyMatrix(matrix* second);
-	void multiplyScalar(double k);
+    void printAll();
+    void printRow(int row);
+    void printCol(int col);
 
-	matrix TransPosedMatrix();
-	matrix AddedMatrix(matrix* second);
-	matrix SubtractedMatrix(matrix* second);
-	matrix MultipledMatrix(matrix* second);
-	matrix MinorMatrix(int row, int col);
-	matrix AdjacentMatrix();
-	matrix InverseMatrix();
-
-	double calculateSarrus();
-	double calculateDeterminant();
-
-	matrix operator + (matrix& source);
-	matrix operator - (matrix& source);
-	matrix operator * (matrix& source);
+    void toUnit();
+    void setValue(int row, int col, double value);
+    void setRand(int row, int col, int max);
+    void fillValue(double value);
+    void fillRand(int max);
+    
+    void transPose();
+    void addMatrix(matrix& second);
+    void subtractMatrix(matrix& second);
+    void multiplyMatrix(matrix& second);
+    void multiplyScalar(double k);
+    
+    matrix TransPosedMatrix();
+    matrix AddedMatrix(matrix& second);
+    matrix SubtractedMatrix(matrix& second);
+    matrix MultipledMatrix(matrix& second);
+    matrix MinorMatrix(int row, int col);
+    matrix AdjacentMatrix();
+    matrix InverseMatrix();
+    
+    double getSarrus();
+    double getDeterminant();
+    
+    matrix operator + (matrix& source);
+    matrix operator - (matrix& source);
+    matrix operator * (matrix& source);
 };
