ed6a9593426dae06208226bc0135ef792b26c3dc
fatpound <84704675+fatpound@users.noreply.github.com>
2023-04-29 21:45:50 +0300

    Update matrix.h

--- Mathematics/Linear Algebra/matrix.h
+++ Mathematics/Linear Algebra/matrix.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <array>
 
-using std::vector, std::array;
+using namespace std;
 
 class matrix
 {
@@ -12,32 +12,40 @@ private:
     vector<vector<double>> _matrix;
     int _row = 0;
     int _col = 0;
-    double determinant = 999979; // A prime number
 
     double calculateSarrus();
     double calculateDeterminant();
 
+    double get_rand(double first, double last);
+
+
 public:
+    matrix();
+    ~matrix();
     matrix(int row, int col);
     matrix(matrix& source);
     matrix(double* arr, double length, int row, int col);
-    matrix(vector<double>, int row, int col);
-    ~matrix();
+    matrix(vector<double> vec, int row, int col);
 
     void printAll();
     void printRow(int row);
     void printCol(int col);
 
     void toUnit();
+    void setValue(int row, int col, int value);
     void setValue(int row, int col, double value);
     void setRand(int row, int col, int max);
+    void setRand(int row, int col, double max);
+    void fillValue(int value);
     void fillValue(double value);
     void fillRand(int max);
+    void fillRand(double max);
     
     void transPose();
     void addMatrix(matrix& second);
     void subtractMatrix(matrix& second);
     void multiplyMatrix(matrix& second);
+    void multiplyScalar(int k);
     void multiplyScalar(double k);
     
     matrix TransPosedMatrix();
