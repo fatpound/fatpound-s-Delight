8bf83c9a0b53a9ab4fa49b567e4022088ffb7eaf
fatpound <84704675+fatpound@users.noreply.github.com>
2023-06-17 18:02:51 +0300

    Update SinglyLL.hpp

--- LinkedList/SinglyLL.hpp
+++ LinkedList/SinglyLL.hpp
@@ -2,265 +2,177 @@
 
 #include "fatpound.hpp"
 
-namespace fatpound
+namespace fatpound::linkedlist
 {
-    namespace linkedlist
+    template <typename T>
+    class SinglyLL
     {
-        template <typename T>
-        class SinglyLL
+    private:
+        class node
         {
         private:
-            class node
-            {
-            private:
 
-
-            protected:
-
-
-            public:
-                SinglyLL<T>::node* next = nullptr;
-                T item;
-
-                node(T new_item);
-            };
-
-            /********************************/
-            /*           Variables          */
-            /********************************/
-
-            SinglyLL<T>::node* list = nullptr;
-            SinglyLL<T>::node* end  = nullptr;
-
-            std::int_fast64_t item_count = 0;
-            
-            /********************************/
-            /*           Functions          */
-            /********************************/
-
-            void connect(SinglyLL<T>& second);
-            void connect_sorted(SinglyLL<T>::node* node);
-            
 
         protected:
 
 
         public:
-            ~SinglyLL();
+            SinglyLL<T>::node* next = nullptr;
+            T item;
 
-            void add(T new_item);
-            void add_sorted(T new_item);
-            void combine(SinglyLL<T>& second);
-            void combine_sorted(SinglyLL<T>& second);
-            void reverse();
-            void list_all();
+            node(T new_item);
         };
 
-        template <typename T> SinglyLL<T>::node::node(T new_item)
-        {
-            this->item = new_item;
-        }
-
-        template <typename T> SinglyLL<T>::~SinglyLL()
-        {
-
-        }
-
         /********************************/
-        /*       Private Functions      */
+        /*           Variables          */
         /********************************/
-        //
-        // Return type : void
-        //
-        template <typename T> void SinglyLL<T>::connect(SinglyLL<T>& second)
+
+        SinglyLL<T>::node* list = nullptr;
+        SinglyLL<T>::node* end  = nullptr;
+
+        std::int_fast64_t item_count = 0;
+        
+
+    protected:
+
+
+    public:
+        ~SinglyLL();
+
+        void add(T new_item);
+        void add_sorted(T new_item);
+        void reverse();
+        void list_all();
+    };
+
+    template <typename T> SinglyLL<T>::node::node(T new_item)
+    {
+        this->item = new_item;
+    }
+
+    template <typename T> SinglyLL<T>::~SinglyLL()
+    {
+
+    }
+
+    /********************************/
+    /*       Public Functions       */
+    /********************************/
+    //
+    // Return type : void
+    //
+    template <typename T> void SinglyLL<T>::add(T new_item)
+    {
+        SinglyLL<T>::node* new_part = new SinglyLL<T>::node(new_item);
+
+        this->item_count++;
+
+        if (this->list == nullptr)
         {
-            SinglyLL<T>::node* end = this->go_to_index(this->item_count - 1);
-            end->next = second.list;
-        }
-        template <typename T> void SinglyLL<T>::connect_sorted(SinglyLL<T>::node* node)
-        {
-            if (this->list == nullptr)
-            {
-                this->list = node;
-                return;
-            }
-
-            if (node->item < this->list->item)
-            {
-                node->next = this->list;
-                this->list = node;
-
-                return;
-            }
-
-            SinglyLL<T>::node* temp = this->list;
-
-            while (temp->next != nullptr)
-            {
-                if (temp->item <= node->item && node->item <= temp->next->item)
-                {
-                    node->next = temp->next;
-                    temp->next = node;
-
-                    return;
-                }
-
-                temp = temp->next;
-            }
-
-            temp->next = node;
-            node->next = nullptr;
-        }
-
-        /********************************/
-        /*       Public Functions       */
-        /********************************/
-        //
-        // Return type : void
-        //
-        template <typename T> void SinglyLL<T>::add(T new_item)
-        {
-            SinglyLL<T>::node* new_part = new SinglyLL<T>::node(new_item);
-
-            this->item_count++;
-
-            if (this->list == nullptr)
-            {
-                this->list = new_part;
-                this->end  = new_part;
-
-                return;
-            }
-
-            this->end->next = new_part;
-            this->end       = new_part;
-        }
-        template <typename T> void SinglyLL<T>::add_sorted(T new_item)
-        {
-            SinglyLL<T>::node* new_part = new SinglyLL<T>::node(new_item);
-
-            this->item_count++;
-
-            if (this->list == nullptr)
-            {
-                this->list = new_part;
-                this->end  = new_part;
-
-                return;
-            }
-
-            if (new_item < this->list->item)
-            {
-                new_part->next = this->list;
-                this->list = new_part;
-
-                return;
-            }
-
-            SinglyLL<T>::node* temp = this->list;
-
-            while (temp->next != nullptr)
-            {
-                if (temp->item <= new_item && new_item <= temp->next->item)
-                {
-                    new_part->next = temp->next;
-                    temp->next = new_part;
-
-                    return;
-                }
-
-                temp = temp->next;
-            }
-
-            temp->next = new_part;
+            this->list = new_part;
             this->end  = new_part;
+
+            return;
         }
-        template <typename T> void SinglyLL<T>::combine(SinglyLL<T>& second)
+
+        this->end->next = new_part;
+        this->end       = new_part;
+    }
+    template <typename T> void SinglyLL<T>::add_sorted(T new_item)
+    {
+        SinglyLL<T>::node* new_part = new SinglyLL<T>::node(new_item);
+
+        this->item_count++;
+
+        if (this->list == nullptr)
         {
-            if (this->list == nullptr || second.list == nullptr)
-                return;
+            this->list = new_part;
+            this->end  = new_part;
 
-            this->connect(second);
-            this->item_count += second.item_count;
-
-            this->end = second.end;
+            return;
         }
-        template <typename T> void SinglyLL<T>::combine_sorted(SinglyLL<T>& second)
+
+        if (new_item < this->list->item)
         {
-            if (this->list == nullptr || second.list == nullptr)
-                return;
+            new_part->next = this->list;
+            this->list = new_part;
 
-            this->item_count += second.item_count;
+            return;
+        }
 
-            if (this->end->item < second.end->item)
-                this->end = second.end;
+        SinglyLL<T>::node* temp = this->list;
 
-            SinglyLL<T>::node* temp = second.list;
-
-            do
+        while (temp->next != nullptr)
+        {
+            if (temp->item <= new_item && new_item <= temp->next->item)
             {
-                SinglyLL<T>::node* temp2 = temp->next;
+                new_part->next = temp->next;
+                temp->next = new_part;
 
-                this->connect_sorted(temp);
-                temp = temp2;
-            }
-            while (temp != nullptr);
-        }
-        template <typename T> void SinglyLL<T>::reverse()
-        {
-            if (this->list == nullptr)
                 return;
-
-            SinglyLL<T>::node* start_backup = this->list;
-
-            SinglyLL<T>::node* t;
-            SinglyLL<T>::node* a = nullptr;
-            SinglyLL<T>::node* x;
-
-            SinglyLL<T>::node* temp = this->list;
-
-            while (true)
-            {
-                t = temp->next;
-                temp->next = a;
-                a = t;
-                x = temp;
-
-                temp = t->next;
-                t->next = x;
-
-                if (temp == nullptr)
-                {
-                    this->list = t;
-                    return;
-                }
-
-                if (temp->next == nullptr)
-                {
-                    temp->next = t;
-                    this->list = temp;
-
-                    return;
-                }
             }
 
-            this->end = start_backup;
+            temp = temp->next;
         }
-        template <typename T> void SinglyLL<T>::list_all()
+
+        temp->next = new_part;
+        this->end  = new_part;
+    }
+
+    template <typename T> void SinglyLL<T>::reverse()
+    {
+        if (this->list == nullptr)
+            return;
+
+        SinglyLL<T>::node* start_backup = this->list;
+
+        SinglyLL<T>::node* t;
+        SinglyLL<T>::node* a = nullptr;
+        SinglyLL<T>::node* x;
+
+        SinglyLL<T>::node* temp = this->list;
+
+        while (true)
         {
-            if (this->list == nullptr)
-                return;
+            t = temp->next;
+            temp->next = a;
+            a = t;
+            x = temp;
 
-            SinglyLL<T>::node* temp = this->list;
+            temp = t->next;
+            t->next = x;
 
-            do
+            if (temp == nullptr)
             {
-                std::cout << temp << '\t' << temp->item << '\t' << temp->next << '\n';
-                temp = temp->next;
+                this->list = t;
+                return;
             }
-            while (temp != nullptr);
 
-            std::cout << '\n';
+            if (temp->next == nullptr)
+            {
+                temp->next = t;
+                this->list = temp;
+
+                return;
+            }
         }
+
+        this->end = start_backup;
+    }
+    template <typename T> void SinglyLL<T>::list_all()
+    {
+        if (this->list == nullptr)
+            return;
+
+        SinglyLL<T>::node* temp = this->list;
+
+        do
+        {
+            std::cout << temp << '\t' << temp->item << '\t' << temp->next << '\n';
+            temp = temp->next;
+        }
+        while (temp != nullptr);
+
+        std::cout << '\n';
     }
 }
