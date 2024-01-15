2381dbf6862873e4184587d6e24fe37801b07f72
fatpound <84704675+fatpound@users.noreply.github.com>
2024-01-15 23:24:02 +0300

    Add files via upload

--- Compression/Huffman.cpp
+++ Compression/Huffman.cpp
@@ -1,66 +1,83 @@
 #include "Huffman.hpp"
 
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+#include <ranges>
+
+namespace rn = std::ranges;
+
 namespace fatpound::compression
 {
     Huffman::~Huffman() noexcept
     {
-        Delete(root);
+        Delete_(root_);
     }
 
     Huffman::Huffman(const std::string& input)
     {
-        std::vector<std::pair<int64_t, char>*> pairs;
+        if (input.length() == 0)
+        {
+            return;
+        }
 
-        for (auto x : input)
+        std::vector<std::pair<int64_t, char>> pairs;
+
+        for (const auto& ch : input)
         {
             int64_t index = -1;
 
-            for (size_t i = 0; i < pairs.size(); ++i)
+            for (size_t i = 0u; i < pairs.size(); ++i)
             {
-                if (pairs[i]->second == x)
+                if (pairs[i].second == ch)
                 {
-                    index = (int64_t)i;
+                    index = i;
                 }
             }
 
             if (index == -1)
             {
-                pairs.push_back(new std::pair<int64_t, char>{ 1LL, x });
+                pairs.emplace_back(1, ch);
             }
             else
             {
-                pairs.at((const size_t)index)->first++;
+                pairs[index].first++;
             }
         }
 
-        std::sort(pairs.begin(), pairs.end(), [](std::pair<int64_t, char>* ref1, std::pair<int64_t, char>* ref2) {return ref1->first < ref2->first; });
+        rn::sort(pairs, [](const auto& a, const auto& b) -> bool { return a.first < b.first; });
 
-        std::deque<Huffman::node*> deque;
+        std::deque<Node*> deque;
+        deque.resize(pairs.size());
 
         for (size_t i = 0; i < pairs.size(); ++i)
         {
-            deque.push_back(new Huffman::node{ pairs[i]->first, pairs[i]->second });
+            deque[i] = new Node(pairs[i].first, pairs[i].second);
         }
 
         const size_t first_size = deque.size();
 
-        for (size_t i = 0; i < first_size - 1; ++i)
+        for (size_t i = 0u; i < first_size - 1u; ++i)
         {
-            Huffman::node* sol = deque[0];
-            Huffman::node* sag = deque[1];
+            Node* const left = deque[0];
+            Node* const right = deque[1];
 
-            Huffman::node* yeni_dugum = new Huffman::node(sol, sag);
+            Node* newnode = new Node(left, right);
 
             deque.pop_front();
             deque.pop_front();
 
-            size_t index = 0;
+            size_t index = 0u;
 
-            for (size_t j = 0; j < deque.size(); ++j)
+            for (size_t j = 0u; j < deque.size(); ++j)
             {
-                if (yeni_dugum->num > deque[index]->num)
+                if (newnode->num_ > deque[index]->num_)
                 {
-                    index = j + 1;
+                    index = j + 1u;
                 }
                 else
                 {
@@ -68,44 +85,40 @@ namespace fatpound::compression
                 }
             }
 
-            deque.insert(deque.begin() + (int64_t)index, yeni_dugum);
+            deque.insert(deque.begin() + index, newnode);
         }
 
-        root = deque[0];
-        node_count = first_size * 2 - 1;
+        root_ = deque[0];
+        node_count_ = first_size * 2 - 1;
 
-        SetBinaryValues(root, "");
-
-        for (auto x : pairs)
-        {
-            delete x;
-        }
+        SetBinaryValues_(root_);
     }
 
-    void Huffman::DeCompress(std::string& bin)
+
+    std::string Huffman::GetDecompressed(const std::string& bin)
     {
         std::string decompressed;
 
-        for (size_t i = 0; i < bin.size(); ++i)
+        for (size_t i = 0u; i < bin.size(); ++i)
         {
             std::string temp = "";
 
             int64_t index = -1;
 
-            for (size_t j = i; j < bin.size() && temp.size() <= max_depth; ++j)
+            for (size_t j = i; j < bin.size() && temp.size() <= max_depth_; ++j)
             {
                 temp += bin[j];
 
-                if (temp.size() < min_depth)
+                if (temp.size() < min_depth_)
                 {
                     continue;
                 }
 
-                for (size_t k = 0; k < results_binary.size(); ++k)
+                for (size_t k = 0; k < results_binary_.size(); ++k)
                 {
-                    if (temp.size() == results_binary[k].size() && std::strcmp(temp.c_str(), results_binary[k].c_str()) == 0)
+                    if (temp == results_binary_[k])
                     {
-                        index = (int64_t)k;
+                        index = k;
 
                         break;
                     }
@@ -113,7 +126,8 @@ namespace fatpound::compression
 
                 if (index != -1)
                 {
-                    decompressed += results_chars.at((const size_t)index);
+                    decompressed += results_chars_[index];
+
                     i += temp.size() - 1;
 
                     break;
@@ -122,106 +136,115 @@ namespace fatpound::compression
 
             if (index == -1)
             {
-                std::cout << "INCORRECT FORMAT!";
-                return;
+                throw std::runtime_error("Incorrect Format!");
             }
         }
 
-        std::cout << "decompressed: " << decompressed << '\n';
+        std::ostringstream oss;
+
+        oss << "Decompressed: " << decompressed << '\n';
 
         size_t size = bin.size();
 
-        double numerator = (double)(size / 8);
+        double numerator = static_cast<double>(size / 8);
 
-        if (size % 8 != 0)
+        if (size % 8u != 0)
         {
             ++numerator;
         }
 
-        double denominator = (double)decompressed.size();
-        double rate = numerator * 100 / denominator;
+        const double denominator = static_cast<double>(decompressed.size());
+        const double rate = numerator * 100 / denominator;
 
-        std::cout << "package count: " << numerator << '\n';
-        std::cout << "compression rate: %" << rate << " ~= " << numerator << '/' << denominator << '\n';
+        oss << "Package count: " << numerator << '\n';
+        oss << "Compression rate: %" << rate << " ~= " << numerator << '/' << denominator << '\n';
+
+        return oss.str();
     }
-    void Huffman::ListPreorder() const
+
+    void Huffman::ListTreePreorder() const
     {
-        std::cout << "output: ";
+        std::cout << "Output : ";
 
-        ListPreorder(root);
-
-        std::cout << '\n';
-    }
-    void Huffman::ListResults()  const
-    {
-        std::cout << "min_depth: " << min_depth << '\n'
-                  << "max_depth: " << max_depth << '\n';
-
-        for (size_t i = 0; i < results_binary.size(); ++i)
-        {
-            std::cout << results_binary[i] << ' '
-                      << results_chars[i] << '\n';
-        }
+        ListTreePreorder_(root_);
 
         std::cout << '\n';
     }
 
-    void Huffman::Delete(node* node)
+    void Huffman::SetBinaryValues_(Node* node, const std::string& str)
     {
         if (node == nullptr)
         {
             return;
         }
 
-        Delete(node->left);
-        Delete(node->right);
+        if (node->left_ == nullptr && node->right_ == nullptr)
+        {
+            results_chars_.push_back(node->k_);
+            results_binary_.push_back(str);
+
+            if (max_depth_ < str.size())
+            {
+                max_depth_ = str.size();
+            }
+
+            if (min_depth_ > str.size())
+            {
+                min_depth_ = str.size();
+            }
+
+            return;
+        }
+
+        SetBinaryValues_(node->left_,  str + "0");
+        SetBinaryValues_(node->right_, str + "1");
+    }
+    void Huffman::ListTreePreorder_(Node* node) const
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+
+        if (node->left_ == nullptr && node->right_ == nullptr)
+        {
+            std::cout << node->k_ << '*' << node->num_ << ' ';
+
+            return;
+        }
+
+        std::cout << node->num_ << ' ';
+
+        ListTreePreorder_(node->left_);
+        ListTreePreorder_(node->right_);
+    }
+    void Huffman::Delete_(Node* node)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+
+        Delete_(node->left_);
+        Delete_(node->right_);
 
         delete node;
     }
-    void Huffman::SetBinaryValues(node* node, const std::string& str)
+
+
+    Huffman::Node::Node(Node* left, Node* right)
+        :
+        left_(left),
+        right_(right),
+        num_(left->num_ + right->num_)
     {
-        if (node == nullptr)
-        {
-            return;
-        }
 
-        if (node->left == nullptr && node->right == nullptr)
-        {
-            results_chars.push_back(node->k);
-            results_binary.push_back(str);
-
-            if (max_depth < str.size())
-            {
-                max_depth = str.size();
-            }
-
-            if (min_depth > str.size())
-            {
-                min_depth = str.size();
-            }
-
-            return;
-        }
-
-        SetBinaryValues(node->left, str + "0");
-        SetBinaryValues(node->right, str + "1");
     }
-    void Huffman::ListPreorder(node* node) const
+    Huffman::Node::Node(int64_t num, char k)
+        :
+        num_(num),
+        k_(k)
     {
-        if (node == nullptr)
-        {
-            return;
-        }
 
-        if (node->left == nullptr && node->right == nullptr)
-        {
-            std::cout << node->k << '*' << node->num << ' ';
-            return;
-        }
-
-        std::cout << node->num << ' ';
-
-        ListPreorder(node->left);
-        ListPreorder(node->right);
     }
 }
--- Compression/Huffman.hpp
+++ Compression/Huffman.hpp
@@ -2,11 +2,8 @@
 
 #pragma warning(disable:4820)
 
-#include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <deque>
 
 namespace fatpound::compression
 {
@@ -24,60 +21,46 @@ namespace fatpound::compression
 
 
     public:
-        void DeCompress(std::string& bin);
-        void ListPreorder() const;
-        void ListResults()  const;
+        std::string GetDecompressed(const std::string& bin);
+
+        void ListTreePreorder() const;
 
 
     protected:
 
 
     private:
-        struct node
+        struct Node
         {
-            node* left = nullptr;
-            node* right = nullptr;
+            Node* left_ = nullptr;
+            Node* right_ = nullptr;
 
-            std::string binary_val;
+            std::string binary_value_;
 
-            int64_t num = 0;
+            int64_t num_ = 0;
 
-            char k = '\0';
+            char k_ = '\0';
 
-            node(int64_t new_num, char new_k)
-                :
-                num{ new_num },
-                k{ new_k }
-            {
-
-            }
-            node(node* sol, node* sag)
-                :
-                left{ sol },
-                right{ sag },
-                num{ sol->num + sag->num }
-            {
-
-            }
+            Node(Node* left, Node* right);
+            Node(int64_t num, char k);
         };
 
 
     private:
-        void Delete(node* node);
-        void SetBinaryValues(node* node, const std::string& str = {});
-        void ListPreorder(node* node) const;
+        void SetBinaryValues_(Node* node, const std::string& str = "");
+        void ListTreePreorder_(Node* node) const;
+        void Delete_(Node* node);
 
         
     private:
-        std::vector<std::string> results_binary;
-        std::vector<char>        results_chars;
+        std::vector<std::string> results_binary_;
+        std::vector<char> results_chars_;
 
-        node* root = nullptr;
+        Node* root_ = nullptr;
 
-        size_t node_count = 0ui64;
-        size_t leaf_count = 0ui64;
+        size_t node_count_ = 0u;
 
-        size_t min_depth = std::numeric_limits<size_t>::max();
-        size_t max_depth = 0ui64;
+        size_t min_depth_ = std::numeric_limits<size_t>::max();
+        size_t max_depth_ = 0u;
     };
 }
