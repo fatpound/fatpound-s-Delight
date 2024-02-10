986aa9d8596cbe967116867cb5f725b3e36643a3
fatpound <84704675+fatpound@users.noreply.github.com>
2024-02-10 12:38:24 +0300

    Add files via upload

--- File/FatFile.hpp
+++ File/FatFile.hpp
@@ -1,22 +1,32 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
+#include <exception>
+#include <stdexcept>
 
 namespace fatpound::file
 {
     static std::pair<std::string, std::string> GetNameAndExtensionFromFilename(const std::string& filename)
     {
-        const auto dot_it = std::find(filename.rbegin(), filename.rend(), '.');
-        size_t dot_index = 0u;
-
-        if (dot_it != filename.rend())
+        if (std::filesystem::is_regular_file(filename))
         {
-            dot_index = dot_it - filename.rbegin() + 1u;
-        }
+            const auto it = std::find(filename.rbegin(), filename.rend(), '.');
+            size_t dot_index = 0u;
 
-        return {
-            std::string(filename.cbegin(), filename.cend() - dot_index),
-            std::string(_strrev(const_cast<char*>(std::string(filename.rbegin(), dot_it).c_str())))
-        };
+            if (it != filename.rend())
+            {
+                dot_index = it - filename.rbegin() + 1u;
+            }
+
+            return {
+                std::string(filename.cbegin(), filename.cend() - dot_index),
+                std::string(_strrev(const_cast<char*>(std::string(filename.rbegin(), it).c_str())))
+            };
+        }
+        else
+        {
+            throw std::invalid_argument("The given path is not pointing to a file!");
+        }
     }
 }
