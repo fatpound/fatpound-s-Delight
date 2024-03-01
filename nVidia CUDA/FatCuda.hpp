ca76bb4843f02486f9ec3aed8bade7878f8d321f
fatpound <84704675+fatpound@users.noreply.github.com>
2024-03-01 11:03:41 +0300

    Update FatCuda.hpp

--- nVidia CUDA/FatCuda.hpp
+++ nVidia CUDA/FatCuda.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
-#if defined(__DEVICE_LAUNCH_PARAMETERS_H__) || \
-    defined(__CUDA_RUNTIME_API_H__)         || \
-    defined(__CUDA_RUNTIME_H__)             || \
-    defined(__cuda_cuda_h__)
+#if (defined(__DEVICE_LAUNCH_PARAMETERS_H__) || \
+     defined(__CUDA_RUNTIME_API_H__)         || \
+     defined(__CUDA_RUNTIME_H__)             || \
+     defined(__cuda_cuda_h__))
+
+#ifndef __DEVICE_LAUNCH_PARAMETERS_H__
+#include "device_launch_parameters.h"
+#endif // ! __DEVICE_LAUNCH_PARAMETERS_H__
 
 #ifndef __CUDA_RUNTIME_API_H__
 #include <cuda_runtime_api.h>
@@ -13,9 +17,10 @@
 #include "cuda_runtime.h"
 #endif // ! __CUDA_RUNTIME_H__
 
-#ifndef __DEVICE_LAUNCH_PARAMETERS_H__
-#include "device_launch_parameters.h"
-#endif // ! __DEVICE_LAUNCH_PARAMETERS_H__
+#ifndef __cuda_cuda_h__
+#include <cuda.h>
+#endif // ! __cuda_cuda_h__
+
 
 #ifndef _MEMORY_
 #include <memory>
