e58cf34a2d7c1775ab5348bb870cae4366e5b41c
fatpound <84704675+fatpound@users.noreply.github.com>
2024-09-05 22:28:55 +0300

    small math update

--- Math/Math.hpp
+++ Math/Math.hpp
@@ -15,11 +15,18 @@ namespace fatpound::math
     template <typename T>
     concept Multiplicable = requires(T var1, T var2)
     {
-        { var1 * var2 } noexcept -> std::same_as<T>;
+        { var1 * var2 } -> std::same_as<T>;
     };
-    
+
+    template <typename T>
+    concept Multiplicable_Noexcept = Multiplicable<T> and
+    requires(T var1, T var2)
+    {
+        { var1 * var2 } noexcept;
+    };
+
     template <Multiplicable T>
-    inline auto Square(const T& var) noexcept
+    inline constexpr auto Square(const T& var) noexcept(Multiplicable_Noexcept<T>)
     {
         return var * var;
     }
